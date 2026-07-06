7d698cdc7f841a4a1658e6e962eb0005aaba360b
jasneetkr-1610 <kjasneet1610@gmail.com>
2026-07-06 12:49:18 +0530

    Two sum problem solved

---
+++ array_and_hashing/0001_two_sum_problem.py
@@ -0,0 +1,12 @@
+
+def twoSum(nums, target):
+    hashmap={}
+    for i,num in enumerate(nums):
+        complement=target-num
+        if complement in hashmap:
+            return [hashmap[complement],i]
+        hashmap[num]=i
+
+nums=[2,7,11,15]
+target=9
+print(twoSum(nums, target))
